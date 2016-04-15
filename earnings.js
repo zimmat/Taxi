1ebc34cc707d806fae5fd69338ecde6aa7449420
zimmat <zimkhitha@projectcodex.co>
2016-04-15 14:48:03 +0200

    completed earnings

---
+++ earnings.js
@@ -0,0 +1,15 @@
+module.exports = function(records,regNo){
+  var profit = {};
+  records.forEach(function(earning){
+    console.log(earning);
+    var RegistrationNumber =  earning.RegistrationNumber;
+    var earnings = earning.Fare * earning.Trips;
+    if(profit[RegistrationNumber] === undefined){
+    profit[RegistrationNumber]= 0;
+    }
+profit[RegistrationNumber] = profit[RegistrationNumber] + earnings;
+
+  });
+  console.log(profit);
+  return profit;
+}
