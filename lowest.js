2de78c7444ae8de6663ffe6f35a8685000194a43
zimmat <zimkhitha@projectcodex.co>
2016-04-19 11:16:14 +0200

    returning minimum number of trips

--- lowest.js
+++ lowest.js
@@ -1,9 +1,10 @@
-module.exports = function(capeTownTaxis){
+module.exports = function(capeTownTaxis) {
   var smallest = 100;
-  capeTownTaxis.forEach(function(key){
-  if(key.Trips < smallest){
-  smallest = key.Trips;
-}
+  capeTownTaxis.forEach(function(key) {
+    if (key.Trips < smallest) {
+      smallest = key.Trips;
+    }
   });
+  console.log(smallest);
   return smallest;
 }
